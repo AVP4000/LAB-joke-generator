25022e15de289ec9cd786badf5dd172582c5819e
Andre Phosarath <andrepho22@gmail.com>
2024-06-29 12:42:38 -0500

    did setup and a worked a little on the joke promise, html, scc files

--- public/main.js
+++ public/main.js
@@ -2,24 +2,17 @@
 // import ViewDirectorBasedOnUserAuthStatus from '../utils/viewDirector';
 import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
+import getJK from '../api/promise';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
-  `;
-  console.warn('YOU ARE UP AND RUNNING!');
+const jokeButton = document.querySelector('#joke-button');
+const jokeWrap = document.querySelector('#joke-wrap');
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+let thisJokeyJoke = '';
 
-  // USE WITH FIREBASE AUTH
-  // ViewDirectorBasedOnUserAuthStatus();
-};
-
-init();
+jokeButton.addEventListener('click', () => {
+  jokeButton.innerHTML = 'I guess the jokes on you';
+  getJK().then((data) => {
+    jokewrap.innerHTML = data.setup;
+    thisJokeyJoke = data.delivery;
+  }
+});
